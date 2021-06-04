7760d9130285223272e9f3bab6688affffe905e8
Rafet Ersoy <rafet.ersoy.cs@gmail.com>
2021-06-04 04:21:55 +0300

    Katman oluşturuldu, iş pozisyonları api'si gerçeklendi
    Katmanlar oluşturuldu. İş pozisyonlarını listeleyecek api gerçeklemesi yapıldı.

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -0,0 +1,24 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "job_titles")
+public class JobTitle {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int job_title_id;
+	
+	@Column(name = "title_name")
+	private String job_title_name;
+}
